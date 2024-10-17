64812194b6927156b636b7c82c78b701a14ca04e
William Owen <william.owen.bsc@gmail.com>
2024-10-17 11:38:37 -0700

    work on npm deployment

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@
 const config = {
   stories: [
     "../stories/WelcomePage.mdx",
-    "../stories/Configure.mdx",
+    "../stories/StartHere.mdx",
     "../stories/**/*.mdx",
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)",
     "../stories/**/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)",
