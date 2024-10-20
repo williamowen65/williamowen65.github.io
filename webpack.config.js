b3d3c976d719808b86f8be57c4ca0774398984d1
William Owen <william.owen.bsc@gmail.com>
2024-10-20 14:51:01 -0700

    Updated npm package version

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ module.exports = {
     module: {
         rules: [
             { test: /\.txt$/, use: 'raw-loader' },
-            // { test: /\.css$/, use: ['style-loader', 'css-loader']},
             { test: /\.css$/, use: ['raw-loader']},
             { test: /\.html$/, use: 'html-loader' }
         ],
