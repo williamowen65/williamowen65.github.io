26c21b64765b781c941233e85e98d3d70b25e3b5
William Owen <william.owen.bsc@gmail.com>
2024-10-21 15:16:03 -0700

    Data mock wrapper refactor

--- stories/utils/DataMockWrapper.js
+++ stories/utils/DataMockWrapper.js
@@ -1,8 +1,6 @@
 export default function dataMockWrapper(story, data){
-    const el = document.createElement('div');
-    el.innerHTML = `${story()}`
     document.addEventListener('DOMContentLoaded', () => {  
         el.querySelector('os-dial').data = data
      })
-    return el
+    return story()
 } 
